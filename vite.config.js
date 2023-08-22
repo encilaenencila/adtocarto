93dce74495ab68bd258773a93e900702495b5b46
encilaenencila <joseph.mesia.encila@gmail.com>
2023-08-22 21:39:09 +0800

    fix base path

--- vite.config.js
+++ vite.config.js
@@ -3,29 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    rewriteBasePath()
-  ],
-  appType: 'mpa'
-  // server:{
-  //   port: 3000,
-  //   host: true
-  // }
-})
-const rewriteBasePath = () => {
-  return {
-    name: 'rewrite-slash-to-index-html',
-    apply: 'serve',
-    enforce: 'post',
-    configureServer(server) {
-      // rewrite / as index.html
-      server.middlewares.use('/', (req, _, next) => {
-        if (req.url === '/') {
-          req.url = '/index.html'
-        }
-        next()
-      })
+  plugins: [react()],
+  base: "./",
+    server: {
+        open: './src/index.html',
+        host: true,
+        port: "8080"
     },
-  }
-}
+})
